a733fbc1c603deb8791823690aea53c5f25d3e5c
jalarconleon <jalarcon@leon.cl>
2022-04-05 18:37:30 -0400

    third commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,55 +15,66 @@ class ApiController extends Controller
     {
         $zipcode = $request->zipcode;
 
-        $EntityZipCodes = ZipCodes::where('d_codigo', '=', $zipcode)->first();
-
-        if($EntityZipCodes !== null)
+        if($zipcode)
         {
-            $settlements = ZipCodes::where('d_codigo', '=', $zipcode)
-                                            ->groupBy('id_asenta_cpcons')
-                                            ->get();
-            
-            $array_settlements = array();
+            $EntityZipCodes = ZipCodes::where('d_codigo', '=', $zipcode)->first();
 
-            foreach ($settlements as $settlement)
+            if($EntityZipCodes !== null)
             {
-                $array_settlement_type = array(
-                                                "name"  => $settlement->d_tipo_asenta
-                                            );
+                $settlements = ZipCodes::where('d_codigo', '=', $zipcode)
+                                                ->groupBy('id_asenta_cpcons')
+                                                ->get();
+                
+                $array_settlements = array();
 
-                array_push($array_settlements, [
-                                                    "key" => (int)$settlement["id_asenta_cpcons"],
-                                                    "name" => strtoupper($settlement["d_asenta"]),
-                                                    "zone_type" => strtoupper($settlement["d_zona"]),
-                                                    "settlement_type" => $array_settlement_type
-                                                ]
-                );
+                foreach ($settlements as $settlement)
+                {
+                    $array_settlement_type = array(
+                                                    "name"  => $settlement->d_tipo_asenta
+                                                );
+
+                    array_push($array_settlements, [
+                                                        "key" => (int)$settlement["id_asenta_cpcons"],
+                                                        "name" => mb_strtoupper($settlement["d_asenta"]),
+                                                        "zone_type" => mb_strtoupper($settlement["d_zona"]),
+                                                        "settlement_type" => $array_settlement_type
+                                                    ]
+                    );
+                }
+                $code = $EntityZipCodes->c_CP != "" ? $EntityZipCodes->c_CP : null;
+                $federal_entity = array(
+                                            "key"  => (int)$EntityZipCodes->c_estado, 
+                                            "name" => mb_strtoupper($EntityZipCodes->d_estado), 
+                                            "code" => $code  
+                                        );
+
+                $municipality = array(
+                                            "key"  => (int)$EntityZipCodes->c_mnpio, 
+                                            "name" => mb_strtoupper($EntityZipCodes->D_mnpio)
+                                        );
+
+
+                $respuesta = array(
+                                    "zip_code"       => $zipcode, 
+                                    "locality"       => mb_strtoupper($EntityZipCodes->d_ciudad),
+                                    "federal_entity" => $federal_entity,
+                                    "settlements"    => $array_settlements,
+                                    "municipality"   => $municipality
+                                );
+
+                //return $respuesta;
+                return response()->json($respuesta, 200);
             }
-            $code = $EntityZipCodes->c_CP != "" ? $EntityZipCodes->c_CP : null;
-            $federal_entity = array(
-                                        "key"  => (int)$EntityZipCodes->c_estado, 
-                                        "name" => strtoupper($EntityZipCodes->d_estado), 
-                                        "code" => $code  
-                                    );
-
-            $municipality = array(
-                                        "key"  => (int)$EntityZipCodes->c_mnpio, 
-                                        "name" => strtoupper($EntityZipCodes->D_mnpio)
-                                    );
-
-
-            $respuesta = array(
-                                "zip_code"       => $EntityZipCodes->d_codigo, 
-                                "locality"       => strtoupper($EntityZipCodes->d_ciudad),
-                                "federal_entity" => $federal_entity,
-                                "settlements"    => $array_settlements,
-                                "municipality"   => $municipality
-                            );
-            return response()->json($respuesta, 200);
+            else
+            {
+                return $null;
+                //return response()->json(null, 404);
+            }   
         }
         else
         {
-            return response()->json(null, 404);
-        }
+            return $null;
+            //return response()->json(null, 404);
+        }        
     }
 }
